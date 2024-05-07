8f0a8e910605eb8c652948bc81e1156b10c89e29
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-07 10:49:53 +0700

    membuat class mahasiswa dengan akses public. menggunakan variabel int nim, prosedur void showNIm, menampilkan "No induk ="

---
+++ pointerDua/pointerDua.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+	int nim;
+	void showNim()
+	{
+		cout << "No Induk = " << nim << endl;
+	}
+};
+
+
+
+
+
+
+
+int main()
+{
+   
+}
