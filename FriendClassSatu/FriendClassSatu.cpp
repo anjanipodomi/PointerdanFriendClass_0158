24b87462638f66071b598340335aeb20009ea0ec
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-07 10:59:37 +0700

    membuat class siswa dan orang dengan akses privat string nama, public void setNama(string pNama), orang berteman dengan siswa

---
+++ FriendClassSatu/FriendClassSatu.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+
+
+int main()
+{
+    
+}
