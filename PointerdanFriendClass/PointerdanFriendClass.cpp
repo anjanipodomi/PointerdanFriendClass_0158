c8bb5998d2b3d96c1a9334e61a6098def14379ed
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-07 10:34:52 +0700

    Add project files.

---
+++ PointerdanFriendClass/PointerdanFriendClass.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    void showName() {
+        cout << "No Induk = " << nim << endl;
+
+    }
+};
+
+
+int main()
+{
+    
+}
